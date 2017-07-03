4fb12a5b8ea1b47eee8e958fb9590005650291c8
Timothy Sullivan <tws15@students.uwf.edu>
2017-07-03 18:07:14 -0500

    Added jquery

--- app/data/index.js
+++ app/data/index.js
@@ -1,9 +1,8 @@
-//let $ = require('jquery') 
-
-$('#rcorners2').on('touchend click', function(){
+$('#rcorners2').on('touchend click', function(event){
 	if($('#light1Icon').attr("src") == 'lightbulb-off.png'){
 		$('#light1Icon').attr("src",'lightbulb-on.png');
-	  } else {
+	} else {
 		$('#light1Icon').attr("src",'lightbulb-off.png');
-	  }
+	}
+	 event.preventDefault();
 });
