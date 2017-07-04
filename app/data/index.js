3d1960ab27eef07512c664bf7c5d2f0fe8e90826
Timothy Sullivan <tws15@students.uwf.edu>
2017-07-03 20:00:56 -0500

    Added mqtt

--- app/data/index.js
+++ app/data/index.js
@@ -1,9 +1,23 @@
-let $ = require('jquery');
+const $ = require('jquery');
+const mqtt = require('mqtt')  
+const client = mqtt.connect('mqtts://iodrop.net',{username:'iodrop',password:'Atomic'})
+
+var connected = false;
+var state = 1;
+
+client.on('connect', function () {
+  connected = true;
+})
+
 $('#rcorners2').on('touchend click', function(event){
 	if($('#light1Icon').attr("src") == 'lightbulb-off.png'){
 		$('#light1Icon').attr("src",'lightbulb-on.png');
+		state = 1;
 	} else {
 		$('#light1Icon').attr("src",'lightbulb-off.png');
+		state = 0;
 	}
 	 event.preventDefault();
+	 
+	if(connected) { client.publish('test', state); }
 });
